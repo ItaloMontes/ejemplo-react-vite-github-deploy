528b97a4b4e8ec48b42e457f49aeebe793a31091
ItaloMontes <italo.montes@gmail.com>
2023-09-11 18:00:24 -0300

    terrr comit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "ejemplo-react-vite-github-deploy",
+  base: "ejemplo-react-vite-github-deploy12345",
 })
